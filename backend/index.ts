fa7b10c2479a5d580c2676b23e83e5f46e667af6
anuj solania <anujsolania0507@gmail.com>
2026-05-18 20:10:03 +0530

    Prompt and steps

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,19 @@ import { express } from "express";
 
 const app = express();
 
-app.post("/chat", async (req, res) => {});
+app.use(express.json());
+
+app.post("/purpexility_ask", async (req, res) => {
+  //get the user query
+  const userQuery = req.body.query;
+
+  //do web search
+
+  //context engineering (web search + some context)
+
+  //hit the LLM api and stream the response
+
+  //end the stream
+});
 
 app.listen(3000);
