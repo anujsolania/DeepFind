19bc746b4ad1a64832b2d5e009ebdfe0f0bbf6f2
anuj solania <anujsolania0507@gmail.com>
2026-05-19 12:49:54 +0530

    first request done

--- backend/index.ts
+++ backend/index.ts
@@ -1,4 +1,10 @@
 import { express } from "express";
+import { tavily } from "@tavily/core";
+import { PROMPT_TEMPLATE } from "./prompt";
+import { Output, streamText } from "ai";
+import z, { url } from "zod";
+
+const client = tavily({ apiKey: process.env.AVILY_API_KEY });
 
 const app = express();
 
@@ -9,10 +15,46 @@ app.post("/purpexility_ask", async (req, res) => {
   const userQuery = req.body.query;
 
   //do web search
+  const webSearchResponse = await client.search(userQuery, {
+    searchDepth: "advanced",
+  });
+
+  const webSearchResults = webSearchResponse.results;
 
   //context engineering (web search + some context)
+  const PROMPT = PROMPT_TEMPLATE.replace(
+    "{{WEB_SEARCH_RESULTS}}",
+    JSON.stringify(webSearchResults)
+  ).replace("{{USER_QUERY}}", userQuery);
 
   //hit the LLM api and stream the response
+  const result = streamText({
+    model: "openai/gpt-5.4",
+    prompt: PROMPT,
+    output: Output.object({
+      schema: z.object({
+        followUps: z.array(z.string()),
+        answers: z.string(),
+      }),
+    }),
+  });
+
+  //required headers
+  res.header("Cache-Control", "no-cache");
+  res.header("Content-Type", "text/event-stream");
+
+  for await (const textPart of result.textStream) {
+    res.write(textPart);
+  }
+
+  //stream back the responses
+  res.write("<SOURCES>");
+
+  res.write(
+    JSON.stringify(webSearchResults.map((result) => ({ url: result.url })))
+  );
+
+  res.write("</SOURCES>");
 
   //end the stream
 });
