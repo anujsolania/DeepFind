c4aaa5f1e5521933d309f101aeec3271de65d123
anuj solania <anujsolania0507@gmail.com>
2026-05-20 21:35:22 +0530

    removed unncessary code

--- backend/test_tavily.ts
+++
@@ -1,7 +0,0 @@
-import { tavily } from "@tavily/core";
-const client = tavily({ apiKey: process.env.TAVILY_API_KEY });
-console.log("Searching...");
-client.search("what is typescript", { searchDepth: "advanced" })
-  .then(res => console.log("Success:", res.results.length))
-  .catch(err => console.error("Error:", err))
-  .finally(() => console.log("Done."));
